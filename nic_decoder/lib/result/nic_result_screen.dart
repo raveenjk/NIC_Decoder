231d875e201a4043b4035f4014bac37fe0971efd
raveenjk <raveenmadhawa48@gmail.com>
2025-03-02 03:12:48 +0530

    Update nic_result_screen.dart

--- nic_decoder/lib/result/nic_result_screen.dart
+++ nic_decoder/lib/result/nic_result_screen.dart
@@ -17,8 +17,17 @@ class NICResultScreen extends StatelessWidget {
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
         title: const Text("Your NIC Details"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         centerTitle: true,
+        leading: IconButton(
+          icon: Image.asset('lib/assets/arrow.png', width: 24, height: 24), // Use custom back button
+          onPressed: () {
+            Get.back();
+          },
+        ),
       ),
+
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
